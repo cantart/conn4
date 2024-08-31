fcd150c21aeef6f43be37ae4c4f3dca50f46a6b4
Sukrit Banthao <arm.xdsk@gmail.com>
2024-08-31 12:05:39 +0900

    update

--- src/lib/OnlineMatch.svelte
+++ src/lib/OnlineMatch.svelte
@@ -2,11 +2,34 @@
 	import type { DocumentReference } from 'firebase/firestore';
 	import { type Game } from './game.svelte';
 	import GameUI from './GameUI.svelte';
+	import { onSnapshot } from 'firebase/firestore';
+	import type { Doc } from './firestore';
+	import { onMount } from 'svelte';
 
 	let props: { game: Game; gameRoomRef: DocumentReference; yourId: string } = $props();
+	let hasPendingWrites = $state(false);
+	let unsub = $state<(() => void) | null>(null);
 
 	$effect(() => {
 		// start listening to the game room
+		unsub = onSnapshot(props.gameRoomRef, (snap) => {
+			if (!snap.exists()) {
+				throw new Error('Game room does not exist');
+			}
+			hasPendingWrites = snap.metadata.hasPendingWrites;
+			if (snap.metadata.hasPendingWrites) {
+				return;
+			}
+
+			const data = snap.data() as Doc['gameRooms'];
+			// listen for changes in the game
+		});
+	});
+
+	onMount(() => {
+		return () => {
+			unsub?.();
+		};
 	});
 </script>
 
@@ -14,6 +37,9 @@
 	game={props.game}
 	isColumnCannotDrop={!!props.game.wonPlayer || props.game.currentPlayerTurn().id !== props.yourId}
 	onDrop={(column) => {
+		if (hasPendingWrites) {
+			return;
+		}
 		// props.socket.send(JSON.stringify({ type: 'drop', column, roomId: props.roomId }));
 	}}
 	onRestart={() => {
