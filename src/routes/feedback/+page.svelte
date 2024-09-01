263bb70b7e13acdc1136bb167b6bc6b284ff629b
Sukrit Banthao <arm.xdsk@gmail.com>
2024-09-01 11:45:58 +0900

    add a feedback page

---
+++ src/routes/feedback/+page.svelte
@@ -0,0 +1,13 @@
+<div style="filter: invert(1);">
+	<div style="filter: hue-rotate(189.73deg) saturate(18.61%) brightness(96.86%)">
+		<iframe
+			title="feedback"
+			src="https://docs.google.com/forms/d/e/1FAIpQLSdQWaIBRZAsaYb29GU-fj7VV35nSxHAc5iOKu2qLkBEFkhb5Q/viewform?embedded=true"
+			width="640"
+			height="640"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0">Loading...</iframe
+		>
+	</div>
+</div>
