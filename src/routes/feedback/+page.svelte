61a69f04e2fdbff8159433fe336e6ebef3b9d79a
Sukrit Banthao <arm.xdsk@gmail.com>
2024-09-01 12:25:45 +0900

    adjust feedback form width

--- src/routes/feedback/+page.svelte
+++ src/routes/feedback/+page.svelte
@@ -1,9 +1,12 @@
-<div style="filter: invert(1);">
-	<div style="filter: hue-rotate(189.73deg) saturate(18.61%) brightness(96.86%)">
+<div style="filter: invert(1);" class="w-screen">
+	<div
+		style="filter: hue-rotate(189.73deg) saturate(18.61%) brightness(96.86%)"
+		class="grid place-items-center"
+	>
 		<iframe
+			class="w-full"
 			title="feedback"
 			src="https://docs.google.com/forms/d/e/1FAIpQLSdQWaIBRZAsaYb29GU-fj7VV35nSxHAc5iOKu2qLkBEFkhb5Q/viewform?embedded=true"
-			width="640"
 			height="640"
 			frameborder="0"
 			marginheight="0"
