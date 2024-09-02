890b0d21356fee7b9d4075d4cb8cb77a4e933ea3
Sukrit Banthao <arm.xdsk@gmail.com>
2024-09-02 20:04:22 +0900

    connect to firebase emulator when on local

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import type { FirebaseApp } from 'firebase/app';
-import { getFirestore, type Firestore } from 'firebase/firestore';
+import type { FirebaseApp, FirebaseOptions } from 'firebase/app';
+import { connectFirestoreEmulator, getFirestore, type Firestore } from 'firebase/firestore';
 import type { Auth } from 'firebase/auth';
 import { browser } from '$app/environment';
 
@@ -9,13 +9,13 @@ export let db: Firestore;
 export let app: FirebaseApp;
 export let auth: Auth;
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
 	apiKey: 'AIzaSyAXsv_EdGzJw4UYCDdkLMnXEyEb_lZIz1s',
 	authDomain: 'fial-the-game.firebaseapp.com',
 	projectId: 'fial-the-game',
 	storageBucket: 'fial-the-game.appspot.com',
 	messagingSenderId: '9162154050',
-	appId: '1:9162154050:web:485c1425bedcb0bc098ddb'
+	appId: '1:9162154050:web:485c1425bedcb0bc098ddb',
 };
 
 export const initializeFirebase = () => {
@@ -26,6 +26,9 @@ export const initializeFirebase = () => {
 		app = initializeApp(firebaseConfig);
 		auth = getAuth(app);
 		db = getFirestore(app);
+		if (browser) {
+			connectFirestoreEmulator(db, '127.0.0.1', 8080);
+		}
 	}
 };
 
