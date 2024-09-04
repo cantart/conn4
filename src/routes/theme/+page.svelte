034d00957be3360de67d3a9fa25bf0427939d8ea
Sukrit Banthao <arm.xdsk@gmail.com>
2024-09-04 22:19:33 +0900

    show color pallet to the themes

--- src/routes/theme/+page.svelte
+++ src/routes/theme/+page.svelte
@@ -2,7 +2,7 @@
 	import { themes } from '$lib/themes';
 </script>
 
-<div class="flex max-w-96 flex-wrap justify-center gap-2 transition-all">
+<div class="flex max-w-[44rem] flex-wrap justify-center gap-2 transition-all">
 	{#each themes as t}
 		<button
 			data-theme={t}
@@ -10,7 +10,15 @@
 			onclick={() => {
 				document.documentElement.setAttribute('data-theme', t);
 				localStorage.setItem('theme', t);
-			}}>{t}</button
+			}}
+		>
+			<div class="flex -space-x-2">
+				<div class="aspect-square w-4 rounded-full bg-neutral"></div>
+				<div class="aspect-square w-4 rounded-full bg-accent"></div>
+				<div class="aspect-square w-4 rounded-full bg-secondary"></div>
+				<div class="aspect-square w-4 rounded-full bg-primary"></div>
+			</div>
+			{t}</button
 		>
 	{/each}
 </div>
