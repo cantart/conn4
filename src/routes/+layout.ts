e7693dbefaf79862d8ef9c2cc2a952a3e2870ec6
Sukrit Banthao <arm.xdsk@gmail.com>
2024-09-04 22:20:09 +0900

    remove comment

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,8 @@
 import { initializeFirebase } from '$lib/firebase.client';
 import { browser } from '$app/environment';
-// import { themeChange } from 'theme-change';
 
 export async function load() {
 	if (browser) {
-		// themeChange(false);
-
 		try {
 			initializeFirebase();
 		} catch (ex) {
