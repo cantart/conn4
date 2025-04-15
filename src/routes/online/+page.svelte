d2d687feb772881e7ba20541187da2a5c7ebb8bc
Arm Sukrit <arm.xdsk@gmail.com>
2025-04-16 00:58:24 +0700

    first working version of google login

--- src/routes/online/+page.svelte
+++ src/routes/online/+page.svelte
@@ -1,4 +1,64 @@
 <script lang="ts" module>
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	import { getAuth, onAuthStateChanged, signInWithPopup, type User, signOut } from 'firebase/auth';
+	import { GoogleAuthProvider } from 'firebase/auth';
+
+	let firebaseUser = $state<
+		| {
+				ready: false;
+		  }
+		| {
+				ready: true;
+				value: User | null;
+		  }
+	>({
+		ready: false
+	});
+
+	const provider = new GoogleAuthProvider();
+
+	const firebaseConfig = {
+		apiKey: 'AIzaSyATTntrsu3XgqR1S73O_FemmFgAwkAw420',
+		authDomain: 'conn4-a3b1f.firebaseapp.com',
+		projectId: 'conn4-a3b1f',
+		storageBucket: 'conn4-a3b1f.firebasestorage.app',
+		messagingSenderId: '255108508987',
+		appId: '1:255108508987:web:2d3b39a1fb097f8ab3db1e'
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const auth = getAuth(app);
+	onAuthStateChanged(auth, async (user) => {
+		if (!firebaseUser.ready) {
+			// Set up conn for the first time
+			if (user) {
+				const idToken = await user.getIdToken();
+				conn = DbConnection.builder()
+					.withUri(import.meta.env.VITE_SPACETIME_DB_URI ?? 'ws://localhost:3000')
+					.withModuleName(import.meta.env.VITE_SPACETIME_DB_MODULE ?? 'fial')
+					.withToken(idToken)
+					.onConnect(onConnect)
+					.onConnectError(onConnectError)
+					.build();
+				console.log('idToken', idToken);
+			} else {
+				conn = DbConnection.builder()
+					.withUri(import.meta.env.VITE_SPACETIME_DB_URI ?? 'ws://localhost:3000')
+					.withModuleName(import.meta.env.VITE_SPACETIME_DB_MODULE ?? 'fial')
+					.withToken('')
+					.onConnect(onConnect)
+					.onConnectError(onConnectError)
+					.build();
+			}
+		}
+		firebaseUser = {
+			ready: true,
+			value: user
+		};
+	});
+
 	let players = new SvelteMap<bigint, Player>();
 	let you = $state<You | null>(null);
 	let yourJoinRoom = $state<JoinRoom | null>(null);
@@ -22,6 +82,10 @@
 		if (!you) {
 			throw new Error('You are not set yet');
 		}
+		if (!conn) {
+			throw new Error('Connection is not set yet');
+		}
+
 		if (jr.joiner.data === you.identity.data) {
 			yourJoinRoom = jr;
 			conn.db.joinRoom.removeOnInsert(youJoinRoomOnInsert);
@@ -32,9 +96,17 @@
 
 	const setupYourJoinRoom = async (y: You) => {
 		return new Promise<void>((resolve) => {
+			if (!conn) {
+				throw new Error('Connection is not set yet');
+			}
+
 			yourJoinRoomHandle = conn
 				.subscriptionBuilder()
 				.onApplied(() => {
+					if (!conn) {
+						throw new Error('Connection is not set yet');
+					}
+
 					for (const jr of conn.db.joinRoom.iter()) {
 						if (jr.joiner.data === you?.identity.data) {
 							yourJoinRoom = jr;
@@ -55,9 +127,8 @@
 		console.error('Error connecting to SpacetimeDB:', error);
 	};
 
-	const onConnect = (conn: DbConnection, identity: Identity, token: string) => {
-		localStorage.setItem('auth_token', token);
-
+	const onConnect = (conn: DbConnection, identity: Identity) => {
+		console.log('identity', identity);
 		conn
 			.subscriptionBuilder()
 			.onApplied(async () => {
@@ -91,13 +162,8 @@
 		});
 	};
 
-	const conn = DbConnection.builder()
-		.withUri(import.meta.env.VITE_SPACETIME_DB_URI ?? 'ws://localhost:3000')
-		.withModuleName(import.meta.env.VITE_SPACETIME_DB_MODULE ?? 'fial')
-		.withToken(localStorage.getItem('auth_token') || '')
-		.onConnect(onConnect)
-		.onConnectError(onConnectError)
-		.build();
+	// svelte-ignore non_reactive_update
+	let conn: DbConnection | null = null;
 </script>
 
 <script lang="ts">
@@ -115,8 +181,6 @@
 	import type { RoomData } from './types';
 	import Room from './Room.svelte';
 
-	$inspect(yourJoinRoom);
-
 	let home: Home | null = $state(null);
 	$effect(() => {
 		if (yourJoinRoom) {
@@ -128,6 +192,10 @@
 			yourJoinRoom = null;
 			if (yourJoinRoomHandle?.isActive()) {
 				yourJoinRoomHandle.unsubscribeThen(async () => {
+					if (!conn) {
+						throw new Error('Connection is not set yet');
+					}
+
 					conn.db.joinRoom.removeOnInsert(youJoinRoomOnInsert);
 					yourJoinRoomHandle = null;
 					await home?.stopUseRooms();
@@ -138,11 +206,56 @@
 	});
 </script>
 
-{#if s.page === 'init'}
+{#if firebaseUser.ready}
+	<!-- content here -->
+	{#if firebaseUser.value}
+		<p>{firebaseUser.value.displayName}</p>
+		<button
+			onclick={() => {
+				conn?.disconnect();
+				conn = DbConnection.builder()
+					.withUri(import.meta.env.VITE_SPACETIME_DB_URI ?? 'ws://localhost:3000')
+					.withModuleName(import.meta.env.VITE_SPACETIME_DB_MODULE ?? 'fial')
+					.withToken('')
+					.onConnect(onConnect)
+					.onConnectError(onConnectError)
+					.build();
+				signOut(auth);
+			}}
+		>
+			Sign out
+		</button>
+	{:else}
+		<button
+			onclick={() => {
+				signInWithPopup(auth, provider)
+					.then(async (result) => {
+						// This gives you a Google Access Token. You can use it to access the Google API.
+						conn?.disconnect();
+						conn = DbConnection.builder()
+							.withUri(import.meta.env.VITE_SPACETIME_DB_URI ?? 'ws://localhost:3000')
+							.withModuleName(import.meta.env.VITE_SPACETIME_DB_MODULE ?? 'fial')
+							.withToken(await result.user.getIdToken())
+							.onConnect(onConnect)
+							.onConnectError(onConnectError)
+							.build();
+						// console.log('credential.idToken', credential.idToken);
+					})
+					.catch((error) => {
+						console.error('Error signing in with Google:', error);
+					});
+			}}>Continue with Google</button
+		>
+	{/if}
+{:else}
+	<!-- else content here -->
+{/if}
+
+{#if s.page === 'init' || !you || !conn}
 	<h1>Connecting...</h1>
-{:else if s.page === 'home' && you}
+{:else if s.page === 'home'}
 	<Home bind:this={home} {conn} {you} />
-{:else if s.page === 'room' && you}
+{:else if s.page === 'room'}
 	<Room
 		{conn}
 		initialRoomTitle={s.initialRoomTitle}
