dd3ab00f2554cdbb84d5a44075d29ba5d8261628
Arm Sukrit <arm.xdsk@gmail.com>
2025-04-26 03:40:12 +0700

    final touches

--- src/routes/online/leaderboard/+page.svelte
+++ src/routes/online/leaderboard/+page.svelte
@@ -6,8 +6,9 @@
 	let conn = $derived(getContext<CustomerContext['conn']>('conn')());
 	const connected = getContext<CustomerContext['connected']>('connected');
 	const players = getContext<CustomerContext['players']>('players');
+	let you = $derived(getContext<CustomerContext['you']>('you')());
 </script>
 
-{#if conn && connected()}
-	<Leaderboard {conn} players={players()} />
+{#if conn && connected() && you}
+	<Leaderboard {conn} players={players()} {you} />
 {/if}
--- src/routes/online/leaderboard/Leaderboard.svelte
+++ src/routes/online/leaderboard/Leaderboard.svelte
@@ -6,8 +6,11 @@
 	import { m } from '$lib/paraglide/messages';
 	import { flip } from 'svelte/animate';
 	import { useCrossfade } from '$lib/transitions';
+	import PlayerNameWithStatus from '../PlayerNameWithStatus.svelte';
+	import type { You } from '$lib';
 
-	let { conn, players }: { conn: DbConnection; players: SvelteMap<bigint, Player> } = $props();
+	let { conn, players, you }: { conn: DbConnection; players: SvelteMap<bigint, Player>; you: You } =
+		$props();
 	const lb = new UseLeaderboard(conn);
 
 	const headColumns = [
@@ -28,7 +31,7 @@
 
 <div class="space-y-2 overflow-x-auto">
 	<div class="flex justify-center gap-2">
-		<h1>Last Month</h1>
+		<h1>{m.witty_few_owl_amaze()}</h1>
 		<a href="/online" class="btn btn-xs btn-error btn-outline">✈️ {m.sleek_strong_turtle_pat()}</a>
 	</div>
 	<table class="table">
@@ -48,14 +51,28 @@
 				</tr>
 			{:else}
 				{#each lb.oneMonth.data as item, i (item.player.data)}
-					{@const name = players.get(item.player.data)?.name ?? '(Unknown)'}
+					{@const p = players.get(item.player.data)}
+					{@const nameStatus = p
+						? { name: p.name ?? '(???)', online: p.online }
+						: { name: '(???)', online: false }}
+					{@const isYou = you.identity.data === item.player.data}
 					<tr
 						animate:flip
 						in:receive={{ key: item.player.data }}
 						out:send={{ key: item.player.data }}
 					>
 						<th>{i + 1}</th>
-						<td>{name}</td>
+						<td
+							><div
+								data-tip="You"
+								class={{
+									'font-semibold': isYou,
+									tooltip: isYou
+								}}
+							>
+								<PlayerNameWithStatus name={nameStatus.name} online={nameStatus.online} />
+							</div></td
+						>
 						<td>{(item.winRate * 100).toFixed(2) + '%'}</td>
 						<td>{item.wins} / {item.total}</td>
 					</tr>
