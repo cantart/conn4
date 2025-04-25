067eab5c71bff288162563cc02c57dbc853ce673
Arm Sukrit <arm.xdsk@gmail.com>
2025-04-25 23:28:20 +0700

    update api of `UseLeaderboard`

--- src/routes/online/UseLeaderboard.svelte.ts
+++ src/routes/online/UseLeaderboard.svelte.ts
@@ -3,36 +3,44 @@ import type { DbConnection, EventContext } from "../../module_bindings";
 import type { SubscriptionHandle } from '$lib';
 
 export class UseLeaderboard {
-    public oneMonth = $state<StatsOneMonth[] | null>(null);
+    public oneMonth = $state<{
+        loading: true
+    } | {
+        loading: false, data: StatsOneMonth[]
+    }>({ loading: true });
 
     private readonly oneMonthSubHandle: SubscriptionHandle
     private readonly oneMonthOnInsert: (ctx: EventContext, row: StatsOneMonth) => void = (ctx, row) => {
-        this.oneMonth?.push(row)
+        if (this.oneMonth.loading) {
+            console.error('One month stats not set yet')
+            return
+        }
+        this.oneMonth.data.push(row)
     }
     private readonly oneMonthOnUpdate: (ctx: EventContext, oldRow: StatsOneMonth, newRow: StatsOneMonth) => void = (ctx, oldRow, newRow) => {
-        if (!this.oneMonth) {
+        if (this.oneMonth.loading) {
             console.error('One month stats not set yet')
             return
         }
 
-        const idx = this.oneMonth.findIndex((r) => r.player.data === oldRow.player.data)
+        const idx = this.oneMonth.data.findIndex((r) => r.player.data === oldRow.player.data)
         if (idx !== -1) {
-            this.oneMonth[idx] = newRow
+            this.oneMonth.data[idx] = newRow
         } else {
             console.error('Update for non-existing row??', oldRow, newRow)
         }
     }
     private readonly oneMonthOnDelete: (ctx: EventContext, row: StatsOneMonth) => void = (ctx, row) => {
-        if (!this.oneMonth) {
+        if (this.oneMonth.loading) {
             console.error('One month stats not set yet')
             return
         }
-        const idx = this.oneMonth.findIndex((r) => r.player.data === row.player.data)
+        const idx = this.oneMonth.data.findIndex((r) => r.player.data === row.player.data)
         if (idx === -1) {
             console.error('Delete for non-existing row??', row)
             return
         }
-        this.oneMonth.splice(idx, 1)
+        this.oneMonth.data.splice(idx, 1)
     }
 
     constructor(private readonly conn: DbConnection) {
@@ -41,7 +49,9 @@ export class UseLeaderboard {
 
     private readonly setupOneMonth = () => {
         return this.conn.subscriptionBuilder().onApplied(() => {
-            this.oneMonth = Array.from(this.conn.db.statsOneMonth.iter())
+            this.oneMonth = {
+                loading: false, data: Array.from(this.conn.db.statsOneMonth.iter())
+            }
             this.conn.db.statsOneMonth.onInsert(this.oneMonthOnInsert)
             this.conn.db.statsOneMonth.onUpdate(this.oneMonthOnUpdate)
             this.conn.db.statsOneMonth.onDelete(this.oneMonthOnDelete)
