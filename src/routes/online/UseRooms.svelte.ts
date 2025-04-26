da09b669150116ffbf2b7cc1fc38fd75bb696c09
Arm Sukrit <arm.xdsk@gmail.com>
2025-04-26 14:21:41 +0700

    change the sort order of rooms

--- src/routes/online/UseRooms.svelte.ts
+++ src/routes/online/UseRooms.svelte.ts
@@ -9,43 +9,42 @@ export class UseRooms {
 
     private readonly roomSubHandle: SubscriptionHandle;
 
-    private readonly roomOnInsert: (ctx: EventContext, room: Room) => void
-    private readonly roomOnUpdate: (ctx: EventContext, oldRow: Room, newRow: Room) => void
-    private readonly roomOnDelete: (ctx: EventContext, room: Room) => void
+    private readonly sortRooms = () => {
+        this._rooms.sort((a, b) => {
+            return b.createdAt.toDate().getTime() - a.createdAt.toDate().getTime();
+        });
+    }
+
+    private readonly roomOnInsert: (ctx: EventContext, room: Room) => void = (ctx, room) => {
+        // check if room already exists in the list
+        let existingRoom = this._rooms.find((r) => r.id === room.id);
+        if (existingRoom) {
+            // update the existing room
+            existingRoom = room;
+            return;
+        }
+        this._rooms.push(room);
+        this.sortRooms();
+    }
+    private readonly roomOnUpdate: (ctx: EventContext, oldRow: Room, newRow: Room) => void = (ctx, _, n) => {
+        const index = this._rooms.findIndex((r) => r.id === n.id);
+        if (index !== -1) {
+            this._rooms[index] = n;
+        }
+    }
+    private readonly roomOnDelete: (ctx: EventContext, room: Room) => void = (ctx, room) => {
+        this._rooms = this._rooms.filter((r) => r.id !== room.id);
+    }
 
     constructor(private readonly conn: DbConnection) {
-        this.roomOnInsert = (ctx, room) => {
-            // check if room already exists in the list
-            let existingRoom = this._rooms.find((r) => r.id === room.id);
-            if (existingRoom) {
-                // update the existing room
-                existingRoom = room;
-                return;
-            }
-            this._rooms.push(room);
-            this._rooms.sort((a, b) => {
-                return a.createdAt.toDate().getTime() - b.createdAt.toDate().getTime();
-            });
-        }
-        this.roomOnDelete = (ctx, room) => {
-            this._rooms = this._rooms.filter((r) => r.id !== room.id);
-        }
-        this.roomOnUpdate = (ctx, _, n) => {
-            const index = this._rooms.findIndex((r) => r.id === n.id);
-            if (index !== -1) {
-                this._rooms[index] = n;
-            }
-        }
-
         conn.db.room.onInsert(this.roomOnInsert);
         conn.db.room.onDelete(this.roomOnDelete);
         conn.db.room.onUpdate(this.roomOnUpdate);
         this.roomSubHandle = conn
             .subscriptionBuilder()
             .onApplied((ctx) => {
-                this._rooms = [...ctx.db.room.iter(), ...this._rooms].sort((a, b) => {
-                    return a.createdAt.toDate().getTime() - b.createdAt.toDate().getTime();
-                })
+                this._rooms = [...ctx.db.room.iter(), ...this._rooms]
+                this.sortRooms();
             })
             .onError((ctx) => {
                 console.error('Error fetching rooms:', ctx.event);
